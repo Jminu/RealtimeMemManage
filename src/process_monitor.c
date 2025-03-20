e584ab7b11691b93fff2c07623299da71feb831f
Jin Minu <s9430939@naver.com>
2025-03-21 01:37:08 +0900

    Implements Func that print all processes

---
+++ src/process_monitor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+void monitor_all_process()
+{
+	DIR *dir = opendir("/proc");
+	if(dir == NULL)
+	{
+		perror("opendir error");
+		exit(1);
+	}
+
+	struct dirent *all_process;
+	
+	while((all_process = readdir(dir)) != NULL)
+	{
+		if(all_process->d_type == DT_DIR)
+		{
+			pid_t pid = atoi(all_process->d_name);
+			if(pid > 0)
+			{
+				printf("[Monitor] PID : %d\n", pid);
+			}
+		}
+	}
+	closedir(dir);
+}
+
+int main()
+{
+	monitor_all_process();
+	return 0;
+}
