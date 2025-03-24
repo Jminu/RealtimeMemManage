21f52a96a76d9466e509d21420400c2a74772220
Jin Minu <s9430939@naver.com>
2025-03-25 04:59:54 +0900

    Implements get systemcall number

--- src/ptrace_process.c
+++ src/ptrace_process.c
@@ -5,42 +5,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/user.h>
-#include <linux/elf.h>
+#include <linux/elf.h> //basic binary format in Linux
 #include <string.h>
-#include <sys/uio.h>
-#include <asm/ptrace.h>
+#include <sys/uio.h> //for - ptrace(PTRACE_GETREGSET) call, iovec definition
+#include <asm/ptrace.h> //for - struct user_pt_regs
 
-int status;
+int status; //process status
 
 void ptrace_attach_process(pid_t pid)
 {
 	ptrace(PTRACE_ATTACH, pid, NULL, NULL); //chage this pid to trace mode
-	waitpid(pid, &status, 0); //wait till this process stop
+	waitpid(pid, &status, 0); //wait till child process stop
 	printf("Attach to PID: %d\n", pid);
 }
 
 void get_syscall(pid_t pid)
 {
-	struct user_pt_regs regs;
+	struct user_pt_regs regs; //structure that register status when SystemCall ocurred
+	//basically SystemCall number stored to Reg[8] in arm64 Linux
 	
-	struct iovec io = {
-	        .iov_base = &regs,
-	        .iov_len = sizeof(regs)
+	struct iovec io = { //from "sys/uio.h"
+	        .iov_base = &regs, //register pointer
+	        .iov_len = sizeof(regs) //register size
 	};
-	
-	io.iov_base = &regs;
-	io.iov_len = sizeof(regs);
 
-	ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &io);
-	printf("Current system call number: %llu x0: %llu, x1: %llu\n", regs.regs[8], regs.regs[0], regs.regs[1]); //based on aarch64
+	ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &io); //NT_PRSTATUS: select register set
+	//copy process's register set to io
+	
+	printf("Current system call number: %llu x0: %llu, x1: %llu\n", regs.regs[8], regs.regs[0], regs.regs[1]); //based on aarch64(arm64)
 }	
 
 void ptrace_systemcall(pid_t pid)
 {
-        while(WIFSTOPPED(status))
+        while(WIFSTOPPED(status))  //if this process stopped because of ptrace,  WIFSTOPPED(status) returns TRUE
         {
-                ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-                waitpid(pid, &status, 0);
+                ptrace(PTRACE_SYSCALL, pid, NULL, NULL); //stop when enter the SystemCall
+                waitpid(pid, &status, 0); //
                 
                 if(WIFEXITED(status))
                         break;
