882251ad23575606b1dd7b4b3051aff3f9e554ff
Jin Minu <s9430939@naver.com>
2025-03-25 15:19:57 +0900

    implements struct syscall_event and write .gitignore

--- src/memory_tracker.c
+++ src/memory_tracker.c
@@ -1,4 +1,14 @@
-void detect_memory_systemcall(int syscall_num)
+#include <time.h>
+
+typedef struct syscall_event
+{
+	struct timespec timestamp;
+	int syscall_num;
+	char syscall_name[6];
+}SYSCALL_EVENT
+	
+
+void detect_memory_systemcall(int syscall_num, struct user_pt_regs *regs)
 {
 	if(syscall_num == 214)
 	{
@@ -6,7 +16,7 @@ void detect_memory_systemcall(int syscall_num)
 	}
 	else if(syscall_num == 222)
 	{
-		pitnf("[Detected]: mmap\n");
+		printf("[Detected]: mmap\n");
 	}
 	else if(syscall_num == 215)
 	{
