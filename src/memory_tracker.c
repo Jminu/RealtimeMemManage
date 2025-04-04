4819497379d9c1830274d8f61f7c84d2aaeab340
Jin Minu <s9430939@naver.com>
2025-04-04 23:03:09 +0900

    define system call event struct

--- src/memory_tracker.c
+++ src/memory_tracker.c
@@ -5,6 +5,10 @@ typedef struct syscall_event
 	struct timespec timestamp;
 	int syscall_num;
 	char syscall_name[6];
+	size_t cur_vmsize; //current virtual memory comsumed
+	size_t cur_vmrss; //current real memory comsumed
+	size_t cur_vmdata; //heap area
+	size_t cur_mapped; //area that mapping by mmap
 }SYSCALL_EVENT
 	
 
