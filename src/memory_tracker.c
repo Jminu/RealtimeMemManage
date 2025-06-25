7132e220168445b1d3eee1314461a563269f132a
Jin Minu <s9430939@naver.com>
2025-06-25 10:58:32 +0900

    edit return value systemm call filter

--- src/memory_tracker.c
+++ src/memory_tracker.c
@@ -1,16 +1,22 @@
-void filter_memory_systemcall(int syscall_num, struct user_pt_regs *regs)
+int filter_memory_systemcall(int syscall_num, struct user_pt_regs *regs)
 {
 	if(syscall_num == 214)
 	{
 		printf("[Detected]: brk\n");
+		return 1;
 	}
 	else if(syscall_num == 222)
 	{
 		printf("[Detected]: mmap\n");
+		return 1;
 	}
 	else if(syscall_num == 215)
 	{
 		printf("[Detected]: munmap\n");
+		return 1;
+	}
+	else{
+		return 0; // not SystemCall return false;
 	}
 }
 
